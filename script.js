0fccbc558257e5ffcc97c658fc3f32fe9e8ac0aa
mairahumer <53911036+mairahumer@users.noreply.github.com>
2025-09-20 09:59:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1. Write a program that takes a positive integer from user & display the following in your browser.
+let userInput = prompt("Enter a positive integer:");
+
+// a. number
+var numb = 3.4567;
+document.writeln("Number: " + numb + "<br><br>");
+
+// b. round off value of the number
+var roundoff = Math.round(numb);
+document.writeln("Round off Value: " + roundoff + "<br><br>");
+
+// c. floor value of the number
+var floor = Math.floor(numb);
+document.writeln("Floor Value: " + floor + "<br><br>");
+
+// d. ceil value of the number
+var ceil = Math.ceil(numb);
+document.writeln("Ceil Value: " + ceil + "<br><br>");
+
+// 2. Write a program that takes a negative floating point number from user & display the following in your browser.
+let userInput1 = prompt("Enter a negative integer:");
+
+// a. number
+var numb1 = -2.4567;
+document.writeln("Number: " + numb1 + "<br><br>");
+
+// b. round off value of the number
+var roundoff1 = Math.round(numb1);
+document.writeln("Round off Value: " + roundoff1 + "<br><br>");
+
+// c. floor value of the number
+var floor1 = Math.ceil(numb1);
+document.writeln("Ceil Value: " + floor1 + "<br><br>");
+
+// d. ceil value of the number
+var ceil1 = Math.ceil(numb1);
+document.writeln("Ceil Value: " + ceil1 + "<br><br>");
+
+// 3. Write a program that displays the absolute value of  number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+// 4. Write a program that simulates a dice using random() method of JS Math class. Display the value of dice in your
+// browser.:
+var randomNumber1 = 6;
+var randomNumber2 = 7;
+var randomNumber3 = Math.random();
+
+// 5. Write a program that asks the user about his weight. Parse the user input and display his weight in your browser.
+var user = prompt("Enter your Weight in kilograms");
+document.writeln("The weight of user is " + user);
+// Possible user inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+
+
